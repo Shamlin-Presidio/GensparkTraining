7552a992dff618a0986b86df9580385b2ca4da22
Shamlin-Presidio <shamlin.learning@gmail.com>
2025-05-30 17:16:47 +0530

    30/05/25

--- 29-05-2025/AN/BankingApi/obj/Debug/net8.0/BankingApi.AssemblyInfo.cs
+++ 29-05-2025/AN/BankingApi/obj/Debug/net8.0/BankingApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a0a16741c3e1bc1d800e516dc690d6e16ec79eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+476bcdeac5d69cf44b27d4ced82937a94f14ad09")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
