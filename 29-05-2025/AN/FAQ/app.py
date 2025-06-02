9bd1f08f789c48ae345e53edfa3b3a96eef3444a
Shamlin-Presidio <shamlin.learning@gmail.com>
2025-06-02 11:03:26 +0530

    Sorry, I forgot to upload the flask part!

---
+++ 29-05-2025/AN/FAQ/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify
+import pickle
+
+app = Flask(__name__)
+
+# Load model and vectorizer
+with open("faq_model.pkl", "rb") as f:
+    vectorizer, clf = pickle.load(f)
+
+# Map predictions to full answers
+answers = {
+    "create_account": "Use the create account endpoint: /api/Account/create",
+    "deposit": "Use the transaction endpoint with type 'Deposit': /api/Account/transaction",
+    "check_balance": "This feature is under development",
+    "withdraw": "Withdrawal can be done using the same transaction endpoint with type 'Withdraw'."
+}
+
+# @app.route('/faq', methods=['POST'])
+# def answer_question():
+#     data = request.get_json()
+#     question = data.get("question", "").lower()
+
+#     if not question:
+#         return jsonify({"error": "No question provided"}), 400
+
+#     vec = vectorizer.transform([question])
+#     pred = clf.predict(vec)[0]
+
+#     response = answers.get(pred, "Sorry, I don't understand that question. Arranging a connect with our support desk")
+
+#     return jsonify({"answer": response})
+
+@app.route('/faq', methods=['POST'])
+def answer_question():
+    data = request.get_json()
+    question = data.get("question", "").lower()
+
+    if not question:
+        return jsonify({"error": "No question provided"}), 400
+
+    vec = vectorizer.transform([question])
+    probs = clf.predict_proba(vec)
+    max_prob = max(probs[0])
+
+
+    if max_prob < 0.3:
+        response = "Sorry, I don't understand that question. Arranging a connect with support desk."
+    else:
+        pred = clf.predict(vec)[0]
+        response = answers.get(pred, "Sorry, I don't understand that question. Arranging a connect with support desk.")
+
+    return jsonify({"answer": response})
+
+if __name__ == '__main__':
+    app.run(port=5001)
