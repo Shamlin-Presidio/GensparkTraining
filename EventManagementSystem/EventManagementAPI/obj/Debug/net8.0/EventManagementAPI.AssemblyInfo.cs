1a6fbad736a2add931fd6967664950d69c59f3f2
Shamlin-Presidio <shamlin.learning@gmail.com>
2025-06-11 15:06:50 +0530

    completed task

--- EventManagementSystem/EventManagementAPI/obj/Debug/net8.0/EventManagementAPI.AssemblyInfo.cs
+++ EventManagementSystem/EventManagementAPI/obj/Debug/net8.0/EventManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47cbad721c2db151850a22879bb4787e488143c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9fb977280fb7a4c6c5198c3dbeaea92e5308d0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
