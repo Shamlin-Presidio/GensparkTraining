21a15895f10a8a4123762bd9f000472f2ccf76dd
Shamlin-Presidio <shamlin.learning@gmail.com>
2025-06-24 15:26:04 +0530

    nav changes

--- EventManagementSystem/EventManagementAPI/obj/Debug/net8.0/EventManagementAPI.AssemblyInfo.cs
+++ EventManagementSystem/EventManagementAPI/obj/Debug/net8.0/EventManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+564e9e31d8ecb6e68846de6175a5278ec8bc7879")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b65bdfb6d8d7207ea840ec4d71d785662edf171c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
