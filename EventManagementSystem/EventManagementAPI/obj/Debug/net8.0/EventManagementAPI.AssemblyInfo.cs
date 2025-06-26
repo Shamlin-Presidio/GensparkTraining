983c26fb77ee85795216357411ddf2a7f7593366
Shamlin-Presidio <shamlin.learning@gmail.com>
2025-06-26 15:08:42 +0530

    linux q8

--- EventManagementSystem/EventManagementAPI/obj/Debug/net8.0/EventManagementAPI.AssemblyInfo.cs
+++ EventManagementSystem/EventManagementAPI/obj/Debug/net8.0/EventManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c904a3b891c9aa376f3b2335e9c804c5d68678f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3adc059f58bc00c77c72d8d624648396499e699")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
