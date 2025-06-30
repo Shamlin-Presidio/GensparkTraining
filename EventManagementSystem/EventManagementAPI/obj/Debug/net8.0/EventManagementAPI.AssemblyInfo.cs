1af0c6507618b538896e69976448f35ede71e337
Shamlin-Presidio <shamlin.learning@gmail.com>
2025-06-30 17:07:09 +0530

    ui updates

--- EventManagementSystem/EventManagementAPI/obj/Debug/net8.0/EventManagementAPI.AssemblyInfo.cs
+++ EventManagementSystem/EventManagementAPI/obj/Debug/net8.0/EventManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5ecd8358ef9ec8a026bdcb9f76ea174dc06c23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd25889952cf72e991ba321d5435b1b27535b0d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
