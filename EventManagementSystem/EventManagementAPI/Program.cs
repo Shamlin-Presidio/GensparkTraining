0bb8886589fdfe012b07340db8ed86a7ec7c5197
Shamlin-Presidio <shamlin.learning@gmail.com>
2025-07-01 17:42:28 +0530

    containerized api

--- EventManagementSystem/EventManagementAPI/Program.cs
+++ EventManagementSystem/EventManagementAPI/Program.cs
@@ -160,7 +160,7 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
