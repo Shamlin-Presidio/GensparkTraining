52789d8b37ce411df2aee2d64dfc09ea7020a1df
Shamlin-Presidio <shamlin.learning@gmail.com>
2025-07-18 11:57:23 +0530

    push logs

--- EventManagementSystem/EventManagementAPI/obj/Debug/net8.0/EventManagementAPI.AssemblyInfo.cs
+++ EventManagementSystem/EventManagementAPI/obj/Debug/net8.0/EventManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9094a2d25e5cc3c7044498153128770fb4aae638")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5685147befb20148f4bcf8112afc6686160b9a16")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
